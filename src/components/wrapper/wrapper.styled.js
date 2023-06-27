09f60680daea388bf27df17b119d1655146c7d34
saurabh-gaonkar <saurabhg@axioned.com>
2023-06-27 12:09:07 +0530

    Deleted wrapper to add it again with matching case

--- src/components/wrapper/wrapper.styled.js
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const StyledWrapper = styled.div` // styled component for wrapper
-  max-width: ${props => props.theme.desktop};
-  width: 95%;
-  margin: 0 auto;
-`;
